69b07db9aafc17ff30ccfbf5ccfee0e3d1fa637c
NEOZEO64 <neozeo64@pm.me>
2024-02-13 21:47:08 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Info
+
+Using Features:
+    Wished Features:
+    1. Search with unprecise string
+    2. Find list of components
+    3. Select component
+    4. Find more information for component
+
+    Maybe add:
+    1. Tag parts for projects' BOMs
+    2. Wishlist
+
+Maintenance Features:
+- Add components
+- Remove components
+
+...managing components between categories still need to be done manually
+
+
+
+Component Info
+
+- component code / integer code printed on part
+- component category
+- component category labels
+- main component value
+- color
+- stock
+- price
+- comment
+- picture
+- open datasheet shortcut
---
+++ cli.py
@@ -0,0 +1,164 @@
+import json, datetime, os
+  
+
+
+file = "inventory.json"
+run = True
+filterUsed = []
+size = os.get_terminal_size().columns
+
+welcome = "\033[0;37;40m---------------------------------------------\n\033[1;31;40mWELCOME TO YOUR COMPONENTS\033[0;37;40m\n---------------------------------------------\nEnter 'help' for ideas"
+helpText = "\
+Commands:                             \n\
+LS              - list all components \n\
+CATS            - list all categories    \n\
+ADD <cat> <val> - add filter by category & value \n\
+RM <cat> <val>  - clear filter by category & value \n\
+FILTERS         - list current filters \n\
+CLR             - clear whole filter\n\
+SLC <num>       - select component by number in list\n\
+help            - print this text\n\
+stop            - exit the program & save \n\
+save            - save \n\
+"
+
+# LOAD data
+with open(file, "r") as read_file:
+    data = json.load(read_file) # data is now a dictionary containing EVERYTHING
+
+categories = {}
+for cp in data:
+    for k in cp.keys():
+        if k not in categories.keys():
+            categories[k] = 1
+        else:
+            categories[k] += 1
+categories = dict(sorted(categories.items(), key=lambda item: item[1], reverse=True)) # sort categories
+
+# Tool functions
+def printNormal():
+    print("\033[0;37;40m",end="")
+
+def printHighlight():
+    print("\033[1;31;40m",end="")
+
+def save():
+    with open("file.json", "w") as write_file:
+        json.dump(data, write_file)
+
+def help():
+    print(helpText)
+
+def quit():
+    global run
+    run = False
+
+def getTimeStr():
+    now = datetime.datetime.now()
+    text = now.strftime("%d.%m.%y, %H:%M")
+    return text
+
+def getCategories():
+    text = ""
+    for c in categories:
+        text += c 
+        text += "  "
+    print(text)
+
+def filterData():
+    global data, selection
+    finalData = []
+    for cp in data:
+        found = 0
+        for pair in filterUsed:
+            if pair[0] in cp.keys():
+                if cp[pair[0]] == pair[1]:
+                    found += 1
+        if found == len(filterUsed):
+            finalData.append(cp)
+    selection = finalData
+
+def addFilter(category, value):
+    global filterUsed, selection
+    if category not in categories:
+        print("category does not exist")
+        return
+
+    filterUsed.append([category,value.lower()])
+
+    selectionCopy = selection
+    filterData()
+    if selection == []:
+        filterUsed.remove([category,value.lower()])
+        selection = selectionCopy
+        print("no resulting items")
+        
+
+def removeFilter(category, value):
+    global filterUsed
+    filterUsed.remove([category,value.lower()])
+    filterData()
+
+def listFilters():
+    for f in filterUsed:
+        print("{} == {}".format(*f))
+
+
+def printComponent(i):
+    global selection
+    text = ""
+    cp = selection[i]
+    keys = list(cp.keys())
+    vals = list(cp.values())
+    if "comment" in keys:
+        iComment = keys.index("comment")
+        keys[-1], keys[iComment] = keys[iComment], keys[-1]
+        vals[-1], vals[iComment] = vals[iComment], vals[-1]
+    text = ""
+    for v in vals:
+        text += str(v)
+        text += "\t"
+    if len(text) > size:
+        text = text[:size]
+    print(text)
+
+def listSelection():
+    for i in range(len(selection)):
+        printComponent(i)
+    print("Shows {} results".format(len(selection)))
+
+
+
+
+
+cmdMap = {
+    "help" : help,
+    "stop" : quit,
+    "exit" : quit,
+    "quit" : quit,
+    "q" : quit,
+    "cats": getCategories,
+    "add": addFilter,
+    "rm": removeFilter,
+    "ls": listSelection,
+    "filters" : listFilters
+}
+
+selection = data
+
+printNormal()
+print()
+print(welcome)
+while run:
+    printHighlight()
+    cmdText = input(">").lower().split(" ")
+    printNormal()
+    try:
+        cmd = cmdMap[cmdText[0]]
+    except KeyError:
+        print("Command not found")
+        continue
+    try:
+        cmd(*cmdText[1::])
+    except TypeError as t:
+        print("Wrong argument count:", t)
